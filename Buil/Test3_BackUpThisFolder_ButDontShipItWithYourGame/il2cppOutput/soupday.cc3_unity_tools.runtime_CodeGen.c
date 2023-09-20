9225da3769b2ef33e0845ac9cb59ee717ff9f955
PedreGG <pedretegarrido@gmail.com>
2023-09-20 11:56:00 +0200

    fix

---
+++ Buil/Test3_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/soupday.cc3_unity_tools.runtime_CodeGen.c
@@ -0,0 +1,98 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Reallusion.Import.ColliderManager::.ctor()
+extern void ColliderManager__ctor_m64BAC02C30E0C4E2308DB99AD333A465E1F22A3E (void);
+// 0x00000002 System.Void Reallusion.Import.PhysicsSettingsStore::.ctor()
+extern void PhysicsSettingsStore__ctor_m50B444BECBB403EB8F2478DC3583CE4D8F25F488 (void);
+// 0x00000003 System.Void Reallusion.Import.WeightMapper::.ctor()
+extern void WeightMapper__ctor_mA9EE355AE7D3AA2CD201AC1C244098A3E954A786 (void);
+// 0x00000004 System.Void Reallusion.Import.WrinkleRule::.ctor(Reallusion.Import.MaskSet,Reallusion.Import.MaskSide,System.Int32)
+extern void WrinkleRule__ctor_m65CB7362B90982C20EFDD5387D3AF63F679D5CD2 (void);
+// 0x00000005 System.Void Reallusion.Import.WrinkleRuleSet::.ctor(System.Collections.Generic.Dictionary`2<System.String,Reallusion.Import.WrinkleProp>,System.String,System.Single,System.Single)
+extern void WrinkleRuleSet__ctor_mE0BC7B1FA95797A82E83B332E2E3F9F13999100A (void);
+// 0x00000006 System.Void Reallusion.Import.WrinkleConfig::.ctor(System.String,System.Collections.Generic.List`1<Reallusion.Import.WrinkleRuleSet>)
+extern void WrinkleConfig__ctor_m107E537F938DC015642DCC5BFD26807C134E0E9F (void);
+// 0x00000007 System.Void Reallusion.Import.WrinkleManager::BuildRules()
+extern void WrinkleManager_BuildRules_mF3C3162CBF14242D41CCA678945521924375AF68 (void);
+// 0x00000008 System.Void Reallusion.Import.WrinkleManager::BuildConfig(System.Collections.Generic.Dictionary`2<System.String,Reallusion.Import.WrinkleProp>,System.Single)
+extern void WrinkleManager_BuildConfig_mB057C604F9A9D3CDCC1AB42F38A607AF756B7C87 (void);
+// 0x00000009 System.Void Reallusion.Import.WrinkleManager::UpdateBlendShapeIndices()
+extern void WrinkleManager_UpdateBlendShapeIndices_m9593B45ECB92669A3E51ADE14CC8CE7756A5E774 (void);
+// 0x0000000A System.Void Reallusion.Import.WrinkleManager::CheckInit()
+extern void WrinkleManager_CheckInit_mE45B3C459E82278EFC5D204BCF615C364FA55846 (void);
+// 0x0000000B System.Void Reallusion.Import.WrinkleManager::Start()
+extern void WrinkleManager_Start_m83F36990B6075802BA9A12AFD24079CD24D1D9CD (void);
+// 0x0000000C System.Void Reallusion.Import.WrinkleManager::Update()
+extern void WrinkleManager_Update_mE42575DBEBBADB005665E9A56B38D93458245F24 (void);
+// 0x0000000D System.Void Reallusion.Import.WrinkleManager::.ctor()
+extern void WrinkleManager__ctor_m47E6FA891F2CCC567DCFF6E481DA56912053D3C7 (void);
+static Il2CppMethodPointer s_methodPointers[13] = 
+{
+	ColliderManager__ctor_m64BAC02C30E0C4E2308DB99AD333A465E1F22A3E,
+	PhysicsSettingsStore__ctor_m50B444BECBB403EB8F2478DC3583CE4D8F25F488,
+	WeightMapper__ctor_mA9EE355AE7D3AA2CD201AC1C244098A3E954A786,
+	WrinkleRule__ctor_m65CB7362B90982C20EFDD5387D3AF63F679D5CD2,
+	WrinkleRuleSet__ctor_mE0BC7B1FA95797A82E83B332E2E3F9F13999100A,
+	WrinkleConfig__ctor_m107E537F938DC015642DCC5BFD26807C134E0E9F,
+	WrinkleManager_BuildRules_mF3C3162CBF14242D41CCA678945521924375AF68,
+	WrinkleManager_BuildConfig_mB057C604F9A9D3CDCC1AB42F38A607AF756B7C87,
+	WrinkleManager_UpdateBlendShapeIndices_m9593B45ECB92669A3E51ADE14CC8CE7756A5E774,
+	WrinkleManager_CheckInit_mE45B3C459E82278EFC5D204BCF615C364FA55846,
+	WrinkleManager_Start_m83F36990B6075802BA9A12AFD24079CD24D1D9CD,
+	WrinkleManager_Update_mE42575DBEBBADB005665E9A56B38D93458245F24,
+	WrinkleManager__ctor_m47E6FA891F2CCC567DCFF6E481DA56912053D3C7,
+};
+extern void WrinkleRule__ctor_m65CB7362B90982C20EFDD5387D3AF63F679D5CD2_AdjustorThunk (void);
+static Il2CppTokenAdjustorThunkPair s_adjustorThunks[1] = 
+{
+	{ 0x06000004, WrinkleRule__ctor_m65CB7362B90982C20EFDD5387D3AF63F679D5CD2_AdjustorThunk },
+};
+static const int32_t s_InvokerIndices[13] = 
+{
+	8319,
+	8319,
+	8319,
+	1936,
+	1443,
+	3743,
+	8319,
+	3752,
+	8319,
+	8319,
+	8319,
+	8319,
+	8319,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_soupday_cc3_unity_tools_runtime_CodeGenModule;
+const Il2CppCodeGenModule g_soupday_cc3_unity_tools_runtime_CodeGenModule = 
+{
+	"soupday.cc3_unity_tools.runtime.dll",
+	13,
+	s_methodPointers,
+	1,
+	s_adjustorThunks,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
